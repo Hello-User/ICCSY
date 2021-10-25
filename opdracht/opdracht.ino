dd04656115cbc3d26701644d6200924956fc532a
Hello_User <s.soekhlal@aperturect.com>
2021-10-25 12:44:48 +0200

    alles werkt*

--- opdracht/opdracht.ino
+++ opdracht/opdracht.ino
@@ -1,22 +1,52 @@
 #include "functies.h"
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <util/delay.h>
+#include <math.h>
 
 void initUsart();
 void writeString(char* s);
 void initServo();
 void initSensor();
 
-uint16_t distance;
+volatile double distance;
 
 int main() {
+    float Vin;
+    float cm, e;
+    float midden = 19;
+    float P,I,D,PID;
+    float time1 = 1,prevCm,v,prevTime;
+    float set;
+    P = 0;
+    I = 0;
+    D = 0;
     initServo();
     initUsart();
     initSensor();
     PORTB |= (1 << PB0) | (1 << PB1);
-    writeString("Welkom bij het regeltechniek practicum.");
-    sei();
+    ADCSRA |= (1<<ADSC);
     while(1){
-    OCR1A = 1500;
-    writeInt(distance);
+      Vin = (distance*5.0)/1024.0; //Volt conversion. Geen phase shift.
+      cm = 13.0*(1.0/Vin); //conversion to cm. Geen filters.
+      e = (midden - cm);
+      P = 25 * e;
+      v = (cm - prevCm)/(time1*0.001 - prevTime*0.001);
+      D = -10 * v;
+      I = 0.02 * e * time1*0.0001;
+      _delay_ms(50);
+      PID = P + I + D;
+      set = PID + 1350;
+      //Serial.println(I);
+      if (cm > (midden - 1) && cm < (midden + 1)){ 
+        OCR1A = 1350;
+      }
+      else{
+        OCR1A = set;
+      }
+      prevCm = cm;
+      prevTime = time1;
+      time1 += 50;
     }
 }
 void initServo() 
@@ -25,16 +55,15 @@ void initServo()
     TCCR1A |= (1 << WGM11) | ( 1 << COM1A1);
     TCCR1B |= (1 << WGM13) | (1 << CS11);
     ICR1 = 20000;
+    //OCR1A = 1700;
 }
-
 void initSensor() {
-    ADMUX |= (1 << REFS0);
-    ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADIE) | (1 << ADPS2) | (1 << ADSC);  //moet nog op 128 bits
-}
-uint16_t leesSensorWaarde() {
-  distance = ADC;
+    ADMUX |= 0b01000000;
+    ADCSRA = (1 << ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2) | (1<<ADIE); //1024 scale factor
+    sei();
 }
 
-ISR(ADC_Vector){
-   leesSensorWaarde();
+ISR(ADC_vect){
+   distance = ADC;
+   ADCSRA |= (1<<ADSC);
 }
